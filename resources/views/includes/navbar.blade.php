f79ac46ac075c8592b85c2a2cc7baf2559e7d15c
Satriotol <satriotol69@gmail.com>
2019-08-16 11:00:15 +0700

    (satrio)navbar flag

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -8,13 +8,11 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+                    <img src="{{ asset('img/english_flag.png') }}" alt="">
                 </a>
                 <div class="dropdown-menu  dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="#"><img src="{{ asset('img/english_flag.png') }}" alt=""> English</a>
+                    <a class="dropdown-item" href="#"><img src="{{ asset('img/spanish_flag.png') }}" alt=""> Spain</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
