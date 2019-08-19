194a6c2ccd00ceb58141c35d195e88150caf5c68
arifahdip <r1v4hw1to@gmail.com>
2019-08-19 12:20:23 +0700

    (arifah)listproduct

--- resources/views/pages/product/listproduct.bladde.php
+++ resources/views/pages/product/listproduct.bladde.php
@@ -1,5 +1,15 @@
 @extends('layouts.default-sidebar')
 @section('content')
-
+<div class="col-sm-9 col-lg-10">
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </section>
 @stop
