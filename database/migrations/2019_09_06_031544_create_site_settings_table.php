bb3c63bcd6ca3fc8bbd3548ad5e9d07120fb5d17
Satriotol <satriotol69@gmail.com>
2019-09-06 10:20:32 +0700

    (satrio)site_setting

---
+++ database/migrations/2019_09_06_031544_create_site_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiteSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_setting', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('site_name');
+            $table->string('site_logo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_setting');
+    }
+}
