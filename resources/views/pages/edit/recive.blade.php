37c59e8dd37d98271b3c533178652bedbcd8deb3
anggabaeee <53551376+anggabaeee@users.noreply.github.com>
2019-09-11 16:12:56 +0700

    recive (Angga)

--- resources/views/pages/edit/recive.blade.php
+++ resources/views/pages/edit/recive.blade.php
@@ -3,7 +3,9 @@
 <style>
     .table th {
         background-color: #686868;
-        co
+        color: #FFF;
+    }
+
 </style>
 <div class="col-sm-9 col-lg-10">
     <div class="container">
@@ -51,7 +53,7 @@
                     <div class="col-md-4">
                         <strong style="font-weight:bold; color: #F00;">Purchase Order Status<span
                                 style="color: #F00">*</span></strong>
-                                @foreach($purchase_order_status as $p)
+                        @foreach($purchase_order_status as $p)
                         @if ($p->id === $purchase_order->status)
                         <p style="font-weight:bold; color: #F00;">{{$p->nama}}</p>
                         @endif
@@ -66,32 +68,96 @@
                         <table class="table">
                             <thead>
                                 <tr>
-                                    <th width="30%">Product Name</th>
-                                    <th width="30%">Product Code</th>
-                                    <th width="30%">Order Quantity</th>
-                                    <th width="10%">Action</th>
+                                    <th>Product Name</th>
+                                    <th>Product Code</th>
+                                    <th>Order Quantity</th>
+                                    <th>Received Quantity</th>
+                                    <th>Each Cost (SGD)</th>
                                 </tr>
                             </thead>
                             <tbody id="additemwrap">
-                                <tr></tr>
-                                <tr></tr>
-                                <tr></tr>
-                                <tr></tr>
+                                <tr>
+                                @if($purchase_order->purchase_order_items->ordered_qty != null)
+                                    <td>
+                                        {{$purchase_order->purchase_order_items->product_name}}
+                                    </td>
+                                    <td>
+                                        {{$purchase_order->purchase_order_items->product_code}}
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control" value="{{$purchase_order->purchase_order_items->ordered_qty}}">
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control">
+                                    </td>
+                                    <td>
+                                        <input type="number" class="form-control">
+                                    </td>
+                                    @else
+                                    @endif
+                                </tr>
+                                <tr>
+                                  @if($purchase_order->purchase_order_items->ordered_qty != null)
+                                    <td colspan="3">
+                                        <strong>Discount Amount (SGD) : <input type="number" class="form-control" style="display:inline; width:200px;"></strong>
+                                    </td>
+                                    <td colspan="2" style="text-align:right;">
+                                    <strong>Total (SGD) : <input type="number" class="form-control" style="display:inline; width:200px;"></strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="5" style="text-align:right;">
+                                    <strong>Tax (0.00%) (SGD) : <input type="number" class="form-control" style="display:inline; width:200px;"></strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="5" style="text-align:right;">
+                                    <strong>Tax (0.00%) (SGD) : <input type="number" class="form-control" style="display:inline; width:200px;"></strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="5" style="text-align:center;">
+                                    <button type="button" class="btn btn-primary col-sm-2" style="height:75px;">Recive</button>
+                                    </td>
+                                </tr>
+                                @else
+                                <td colspan="3">
+                                        <strong>Discount Amount (SGD) : <input type="number" class="form-control" style="display:inline; width:200px;" readonly></strong>
+                                    </td>
+                                    <td colspan="2" style="text-align:right;">
+                                    <strong>Total (SGD) : <input type="number" class="form-control" style="display:inline; width:200px;" readonly></strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="5" style="text-align:right;">
+                                    <strong>Tax (0.00%) (SGD) : <input type="number" class="form-control" style="display:inline; width:200px;" readonly></strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="5" style="text-align:right;">
+                                    <strong>Tax (0.00%) (SGD) : <input type="number" class="form-control" style="display:inline; width:200px;" readonly></strong>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="5" style="text-align:center;">
+                                    <button type="button" class="btn btn-primary col-sm-2" style="height:75px;">Recive</button>
+                                    </td>
+                                </tr>
+                                @endif
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row" style="margin-top: 15px">
-        <div class="col-md-2">
-            <a href="{{ url('/purchase_order')}}">
-                <button class="btn btn-secondary" style="width: 60%"><i class="fa fa-chevron-left"></i>
-                    Back</button></a>
+        <div class="row" style="margin-top: 15px">
+            <div class="col-md-2">
+                <a href="{{ url('/purchase_order')}}">
+                    <button class="btn btn-secondary" style="width: 60%"><i class="fa fa-chevron-left"></i>
+                        Back</button></a>
+            </div>
         </div>
     </div>
-</div>
 </div>
 </section>
 @stop
